c1f55458256a02ead0fa09ab0ddf34a9b3e17f77
Florian Ludwig <f.ludwig@greyrook.com>
2015-03-07 11:36:58 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='imghash',
+    version='0.0.1',
+    description='',
+    author='Florian Ludwig',
+    author_email='f.ludwig@greyrook.com',
+    packages=['imghash'],
+    install_requires=[
+        'Pillow'
+    ],
+)
