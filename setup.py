566b9ccebae1da0f4066782a53b82eb2d04d8ab3
Florian Ludwig <f.ludwig@greyrook.com>
2020-07-21 11:47:24 +0200

    run black

--- setup.py
+++ setup.py
@@ -2,18 +2,12 @@ from setuptools import setup
 
 
 setup(
-    name='imghash',
-    version='0.0.1',
-    description='',
-    author='Florian Ludwig',
-    author_email='f.ludwig@greyrook.com',
-    packages=['imghash'],
-    entry_points={
-        'console_scripts': [
-            'imghash = imghash:main',
-            ],
-    },
-    install_requires=[
-        'Pillow'
-    ],
+    name="imghash",
+    version="0.0.1",
+    description="",
+    author="Florian Ludwig",
+    author_email="f.ludwig@greyrook.com",
+    packages=["imghash"],
+    entry_points={"console_scripts": ["imghash = imghash:main",],},
+    install_requires=["Pillow"],
 )
